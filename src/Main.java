0e2e25c1b96246f2122f2791b4cd188716cd50e2
Nasq23 <honey.menu23@gmail.com>
2019-09-05 17:21:51 -0400

    add modif game

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         //give a level number 1-4
         //then give an option for each level 1-5
         //at end of game tell user if they survived or not
@@ -9,40 +10,84 @@ public class Main {
         Integer level2;
         Integer level3;
         Integer level4;
-        String cabinet ="";
-        String bedroomdoor ="";
-        String trashbin ="";
-        String mattress ="";
-        String kitchen ="";
+        String cabinet = "";
+        String bedroomdoor = "";
+        String trashbin = "";
+        String mattress = "";
+        String kitchen = "";
+        String soap = "";
+        String linen = "";
+        String cans = "";
+        String bag = "";
+        String ties = "";
+        String peanutbag ="";
+        String salt ="";
+        String sugar ="";
+        String oil = "";
+        String milk = "";
+        String blindfold = "";
+        String towel = "";
+        String pillow = "";
+        String sheets ="";
+        String blinds =  "";
+        String shoes = "";
+        String jeans = "";
+        String rings = "";
+        String bows = "";
+        String vinyl = "";
+        String pants = "";
+        String shirts ="";
         Scanner key = new Scanner(System.in);
+
+
 
         System.out.print("Enter level from 1 to 4: ");
         level1 = key.nextInt();
+        System.out.print("Enter first place to go: ");
+        System.out.print("Enter second place to go: ");
+        System.out.print("Enter third place to go: ");
+
         System.out.print("Enter level from 1 to 4: ");
         level2 = key.nextInt();
+        System.out.print("Enter first place to go: ");
+        System.out.print("Enter second place to go: ");
+        System.out.print("Enter third place to go: ");
+
         System.out.print("Enter level from 1 to 4: ");
         level3 = key.nextInt();
+        System.out.print("Enter first place to go: ");
+        System.out.print("Enter second place to go: ");
+        System.out.print("Enter third place to go: ");
+
         System.out.print("Enter level from 1 to 4: ");
         level4 = key.nextInt();
-        if(level1 == 1){
-            kitchen = "go to soap,go to linen,go to cans";
-            if(level2 == 1){
-                trashbin = "go to bag,go to ties,go to peanutbag";
-            }
-            else if(level2 == 2){
-                cabinet = "go to salt,go to sugar,go to oil,go to milk";
-            }
-            else if (level3 == 3){
-                mattress = "go to blindfold,go to towel,go to pillow";
-            }
-            else if (level4 == 4){
-                bedroomdoor = "go to sheets, go to blinds,go to shoes,go to jeans,grab rings,go to bows,grab vinyl,go to pants,go to shirts";
-            }
-            else {
+        System.out.print("Enter first place to go: ");
+        System.out.print("Enter second place to go: ");
+        System.out.print("Enter fourth place to go: ");
+        System.out.print("Enter fifth place to go: ");
+        System.out.print("Enter sixth place to go: ");
+        System.out.print("Enter seventh place to go: ");
+        System.out.print("Enter eighth place to go: ");
+        System.out.print("Enter nineth place to go: ");
+        {
+            System.out.println("You have survived your levels!");
+        if (level1 == 1) {
+            kitchen = "soap,linen,cans";
+            if (level2 == 1) {
+                trashbin = "bag,ties,peanutbag";
+            } else if (level2 == 2) {
+                cabinet = "salt,sugar,oil,milk";
+            } else if (level3 == 3) {
+                mattress = "blindfold,towel,pillow";
+            } else if (level4 == 4) {
+                bedroomdoor = "sheets,blinds,shoes,eans,rings,bows,vinyl,pants,shirts";
+            } else {
                 System.out.println("Wrong job");
             }
         }
+        {
         System.out.println("Your level is" + kitchen + ", The next place to go to is ");
-
+    }
+ }
     }
 }
